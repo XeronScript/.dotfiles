ffa860d9ad40bf94526e0dc71d7e3b97c64c1dc3
XeronScript <ksasin3@gmail.com>
2024-07-19 14:55:51 +0200

    Add oh-my-zsh config

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,24 @@
+# tmux aliases
+alias td="tmux detach"
+
+# eza aliases
+alias ls='eza --color --icons'
+alias la='eza -a --color --icons'
+alias lt='eza --color --icons -T -L=1'
+alias lta='eza --color --icons -a -T -L=1'
+alias ldr='eza --color --icons -T -L=1 -D'
+alias ldra='eza --color --icons -a -T -L=1 -D'
+alias lf='eza --color --icons -T -L=1 -f'
+alias lfa='eza --color --icons -a -T -L=1 -f'
+
+# tree view with customizable depth
+alias lt1='eza --color --icons -T -L=2'
+alias lt2='eza --color --icons -T -L=3'
+alias lt3='eza --color --icons -T -L=4'
+alias lt4='eza --color --icons -T -L=5'
+
+# Life savers
+alias pip='pip3'
+alias vstop='deactivate'
+alias win='cd /mnt/c/Users/kamil/workspace/'
+alias bat='batcat'
